a4812f47744e6a53d9438b1dbe6d95d55d3e1db7
imaMacDonald <macsdontquit@icloud.com>
2021-08-14 16:12:54 -0400

    Updated logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,7 +5,6 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
 L.circleMarker([34.0522, -118.2437],{
     radius: 300,
     color: "black",
